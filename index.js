3e3f50644844998b2569b10668b8d162baa418b0
khahhy <127372724+khahhy@users.noreply.github.com>
2024-12-21 14:08:41 +0700

    recontruct folder

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
-import { connectDB } from "./config/db.js"; // Adjust the path if necessary
+import { connectDB } from "./config/db.js"; 
+import hbs from "hbs";
 import dotenv from "dotenv";
+
 import userRoutes from "./routes/userRoute.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
+// Tạo `__dirname` cho ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Cấu hình thư mục tĩnh
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+
 app.use(express.json()); // Middleware để parse JSON từ request body
 dotenv.config();
+
 
 // Middleware
 app.use(express.json());
@@ -13,8 +29,22 @@ app.use(express.json());
 // Connect to the database
 connectDB();
 
+// Cấu hình view engine Handlebars
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+
 // Sử dụng các route
+
 app.use("/api/users", userRoutes);
+
+
+app.get("/", (req, res) => {
+    res.render("init", {
+        title: "Threads - Welcome",
+    });
+});
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- public/js/applyTheme.js
+++ public/js/applyTheme.js
@@ -1,4 +1,4 @@
-import { theme } from "../../assets/theme.js";
+import { theme } from "./theme.js";
 
 function applyTheme(theme) {
   const root = document.documentElement;
